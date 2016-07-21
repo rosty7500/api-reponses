61c975471995310c53fac839b15bf308c8376d91
rosty7500 <royston@sjinnovation.com>
2016-07-22 02:25:01 +0530

    connected to QA server

--- API response + DB connect/Db connect + simple create&insert.py
+++ API response + DB connect/Db connect + simple create&insert.py
@@ -1,16 +1,22 @@
 __author__ = 'Roysten'
 
 import MySQLdb as sql
-#db_connect()
-db = sql.connect("localhost","root","root","test")
+import csv
+
+
+#db_connection
+db = sql.connect("52.70.55.105","qatesting","Q88I30X0h1052fC","qatesting_db")
 cursor = db.cursor()
 
 
 query = "SELECT VERSION()"
 cursor.execute(query)
 result = cursor.fetchone()
-print"Database version : %s " % result
+print "Database version : %s " % result
 
+
+
+#create query
 '''
 query1 = """CREATE TABLE CUSTOMER (
          FIRST_NAME  CHAR(20) NOT NULL,
@@ -30,21 +36,31 @@ query2 = """CREATE TABLE EMPLOYEE (
          ID INT )"""
 cursor.execute(query2)'''
 
+'''
+#insert query
 query3 = """INSERT INTO EMPLOYEE(FIRST_NAME,
          LAST_NAME, AGE, SEX, INCOME)
-         VALUES ('Mac', 'Mohan', 21, 'M', 2000)"""
+         VALUES ('Mac', 'Mohan', 1000, 'M', 2000)"""
 cursor.execute(query3)
 db.commit()
 
 query5 = """INSERT INTO EMPLOYEE(FIRST_NAME,
          LAST_NAME, AGE, SEX, INCOME)
-         VALUES ('royston', 'fernandes', 31, 'M', 10)"""
+         VALUES ('royston', 'fernandes', 1001, 'M', 10)"""
 cursor.execute(query5)
 db.commit()
 
+
+#select query
 query4 ="SELECT * FROM EMPLOYEE"
 cursor.execute(query4)
 result4 = cursor.fetchall()
+
+#Writing data to csv file
+out = csv.writer(open("test_output.csv","w"), delimiter=",")
+out.writerows(result4)
+
+#Printing data on the console
 for row in result4:
       fname = row[0]
       lname = row[1]
@@ -52,7 +68,10 @@ for row in result4:
       sex = row[3]
       income = row[4]
       id = row[5]
-      print ("fname=%s,lname=%s,age=%d,sex=%s,income=%d,id=%d" %(fname, lname, age, sex, income, id))
+      output = ("fname=%s,lname=%s,age=%d,sex=%s,income=%d,id=%d" %(fname, lname, age, sex, income, id))
+      print output
 cursor.execute("SELECT COUNT(*) FROM EMPLOYEE")
-print cursor.fetchall()
+#print cursor.fetchall()
+'''
+
 db.close()
